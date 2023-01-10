6f1fda142fdb44f3a5491021470563e90003cf2e
JustAlternate <64951265+JustAlternate@users.noreply.github.com>
2023-01-10 23:18:19 +0100

    Working notification

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-const {app,BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Notification } = require('electron')
 
-const createWindow = () => {
+function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences:{
-      preload: path.join(__dirname,'preload.js')
-    }
+    height: 600
   })
 
   win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
+
+
+function showNotification () {
+  const NOTIFICATION_TITLE = 'Welcome to PokeNotif'
+  const NOTIFICATION_BODY = 'This is your very first PokeNotif notification, make sure to hit the \'OK\' button to proceed to the next notification'
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
+
+app.whenReady().then(createWindow).then(showNotification)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit() // Permet de fermer l'application entierement.
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
