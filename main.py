24c894f1424aca287373793e88220f4edf2d25ec
JustAlternate <lololollololol890@gmail.com>
2023-02-03 10:55:49 +0100

    en python

---
+++ main.py
@@ -0,0 +1,28 @@
+from desktop_notifier import DesktopNotifier, Button
+import asyncio
+import time
+
+
+# LES FUNCTIONS :
+
+def clicked():
+    print("clicked")
+    loop.stop()
+
+
+def dismissed():
+    print("dismissed")
+    loop.stop()
+
+
+notifier = DesktopNotifier()
+
+
+async def makeNotif(title, message, buttons=[], on_clicked=clicked, on_dismissed=dismissed):
+    await notifier.send(title=title, message=message, buttons=buttons, on_clicked=on_clicked, on_dismissed=on_dismissed)
+
+loop = asyncio.get_event_loop()
+
+loop.create_task(makeNotif("PokeNotif", "Welcome to PokeNotif"))
+
+loop.run_forever()
